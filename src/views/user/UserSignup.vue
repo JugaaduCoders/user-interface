9450197b6a27743b8df7bd1f988e31cb5dd6acfe
Prakash Rathour <prakashkumarrathour247@gmail.com>
2024-12-24 01:20:22 +0530

    feat: adds homepage api

--- src/views/user/UserSignup.vue
+++ src/views/user/UserSignup.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import MyButton from "@/components/MyButton.vue";
 import { SignUpFormData } from "@/types/signup";
 import { validateEmail, validatePassword } from "@/utils/regex";
 import { ref } from "vue";
@@ -45,58 +46,56 @@ const submit = () => {
 <template>
   <v-container class="d-flex align-center justify-center">
     <v-container>
-      <v-form @submit.prevent="submit">
-        <v-row justify="center" align="center">
-          <v-col sm="2"> <label for="First Name">First Name</label></v-col>
-          <v-col sm="3">
-            <v-text-field
-              variant="underlined"
-              v-model="formData.firstName"
-              :error="!!errors.firstName"
-              :error-messages="errors.firstName"
-            ></v-text-field>
-          </v-col>
-        </v-row>
+      <v-row justify="center" align="center">
+        <v-col sm="2"> <label for="First Name">First Name</label></v-col>
+        <v-col sm="3">
+          <v-text-field
+            variant="underlined"
+            v-model="formData.firstName"
+            :error="!!errors.firstName"
+            :error-messages="errors.firstName"
+          ></v-text-field>
+        </v-col>
+      </v-row>
 
-        <v-row justify="center" align="center">
-          <v-col sm="2 "> Last Name </v-col>
-          <v-col sm="3    ">
-            <v-text-field
-              variant="underlined"
-              v-model="formData.lastName"
-            ></v-text-field>
-          </v-col>
-        </v-row>
+      <v-row justify="center" align="center">
+        <v-col sm="2 "> Last Name </v-col>
+        <v-col sm="3    ">
+          <v-text-field
+            variant="underlined"
+            v-model="formData.lastName"
+          ></v-text-field>
+        </v-col>
+      </v-row>
 
-        <v-row justify="center" align="center">
-          <v-col sm="2 "> Email </v-col>
-          <v-col sm="3    ">
-            <v-text-field
-              variant="underlined"
-              v-model="formData.email"
-              :error="!!errors.email"
-              :error-messages="errors.email"
-            ></v-text-field>
-          </v-col>
-        </v-row>
+      <v-row justify="center" align="center">
+        <v-col sm="2 "> Email </v-col>
+        <v-col sm="3    ">
+          <v-text-field
+            variant="underlined"
+            v-model="formData.email"
+            :error="!!errors.email"
+            :error-messages="errors.email"
+          ></v-text-field>
+        </v-col>
+      </v-row>
 
-        <v-row justify="center" align="center">
-          <v-col sm="2 "> Password </v-col>
-          <v-col sm="3    ">
-            <v-text-field
-              variant="underlined"
-              v-model="formData.password"
-              :type="'password'"
-              :error="!!errors.password"
-              :error-messages="errors.password"
-            ></v-text-field>
-          </v-col>
-        </v-row>
+      <v-row justify="center" align="center">
+        <v-col sm="2 "> Password </v-col>
+        <v-col sm="3    ">
+          <v-text-field
+            variant="underlined"
+            v-model="formData.password"
+            :type="'password'"
+            :error="!!errors.password"
+            :error-messages="errors.password"
+          ></v-text-field>
+        </v-col>
+      </v-row>
 
-        <v-row justify="center" align="center">
-          <v-btn type="submit"> submit </v-btn>
-        </v-row>
-      </v-form>
+      <v-row justify="center" align="center">
+        <my-button :clickHandler="submit"> submit </my-button>
+      </v-row>
     </v-container>
   </v-container>
 </template>
